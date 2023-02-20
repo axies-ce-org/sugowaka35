208c85d0f83f08ccc256b17255f84f669bb36b78
Kite <ixkaito@gmail.com>
2023-02-20 15:08:52 +0900

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   base: '/sugowaka35',
   outDir: './static',
+  build: {
+    assets: 'assets'
+  },
+  integrations: [tailwind()]
 });
