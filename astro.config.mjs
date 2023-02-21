b047963b74743e35f1b734e83861ee7e1da23fb8
Kite <ixkaito@gmail.com>
2023-02-21 15:14:04 +0900

    Fix some warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from 'astro/config';
-import { config } from './package.json';
-
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: './static',
   site: 'https://copyright-edu.axies.jp/sugowaka35',
-  build: {
-    assets: 'assets',
-  },
 });
