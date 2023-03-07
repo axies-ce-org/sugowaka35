348fcfd8cd662f36cb5fb0f55e2270d6efeb2551
ArisaSadano <arisa.pote.831@gmail.com>
2023-03-07 15:48:42 +0900

    Add animation to Menu

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -13,5 +13,23 @@ for (let i = 0; i < smoothScrollTrigger.length; i++) {
   });
 }
 
-export{}
+export {}
 
+// ハンバーガー
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+const item = document.querySelectorAll('#js-nav a[href^="#"]');
+
+ham.addEventListener('click', function () {
+
+  ham.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+
+item.forEach(function (button) {
+
+  button.addEventListener('click', function() {
+    ham.classList.remove('active');
+    nav.classList.remove('active');
+  })
+});
