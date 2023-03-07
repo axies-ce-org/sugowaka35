c7b18d11ba4f2329030e33c34f86bc7973f90743
ArisaSadano <arisa.pote.831@gmail.com>
2023-03-07 11:48:56 +0900

    Add images to top page, Adjust hover animation

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,17 @@
-const hello = 'hell';
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+
+for (let i = 0; i < smoothScrollTrigger.length; i++) {
+  smoothScrollTrigger[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    let href = smoothScrollTrigger[i].getAttribute('href');
+      let targetElement = document.getElementById(href.replace('#', ''));
+    const target = targetElement.getBoundingClientRect().top;
+    window.scrollTo({
+      top: target,
+      behavior: 'smooth',
+    });
+  });
+}
+
+export{}
+
