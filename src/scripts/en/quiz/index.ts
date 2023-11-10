863e2daf0ca9f6fe5932925c527e8905d2aab7ac
ArisaSadano <arisa.pote.831@gmail.com>
2023-11-10 15:41:25 +0900

    Create quiz page in english (WIP)

---
+++ src/scripts/en/quiz/index.ts
@@ -0,0 +1,160 @@
+import questions from '../../../contents/questions_en';
+
+import {
+  disableChoices,
+  enableChoices,
+  getSelectedChoiceNumber,
+  highlightCorrectChoice,
+  highlightSelectedWrongChoice,
+  resetChoices,
+} from './quizChoice';
+
+import { getCorrectAnswersCount, getAnsweredQuestion, resetAnswers, showAnswer } from './quizAnswer';
+
+import {
+  hideElement,
+  getResultContent,
+  resetCorrectAnswersResultkChildren,
+  resetIllustration,
+  resetResultBlock,
+  resetResultContentBlock,
+  showElement,
+  showResult,
+  updateCorrectAnswersNumberSpan,
+  updateIllustration,
+  updateJudgement,
+  updateMessageBlock,
+} from './quizResult';
+
+import { setBrowserId, setResultData, sendData } from './quizData';
+
+export interface ResultData {
+  [x: string]: {
+    answer: number;
+    isCorrect: boolean;
+  };
+}
+
+let resultData: ResultData = {};
+let isComplete = false;
+
+const POST_ENDPOINT = import.meta.env.PUBLIC_POST_ENDPOINT ?? '';
+
+const answerBlocks = document.querySelectorAll<HTMLDivElement>('.js-answer');
+const container = document.querySelector<HTMLDivElement>('.js-container');
+const choices = document.querySelectorAll<HTMLButtonElement>('.js-choice');
+const resultBlock = document.querySelector<HTMLDivElement>('.js-result');
+const resultContentBlock = document.querySelector<HTMLDivElement>('.js-result-content');
+const correctAnswersBlock = document.querySelector<HTMLDivElement>('.js-correct-answers');
+const allCorrectBlock = document.querySelector<HTMLDivElement>('.js-all-correct');
+const correctAnswersNumberSpan = document.querySelector<HTMLSpanElement>('.js-correct-answers-number');
+const messageBlock = document.querySelector<HTMLDivElement>('.js-message');
+const illustrationImage = document.querySelector<HTMLImageElement>('.js-illustration');
+const submitButton = document.querySelector<HTMLButtonElement>('button[type="submit"]');
+const resetButton = document.querySelector<HTMLButtonElement>('.js-reset');
+
+const onClickChoice = (clickedChoice: HTMLButtonElement) => {
+  const currentQuestion = clickedChoice.closest<HTMLDivElement>('.js-question');
+  const currentChoices = currentQuestion!.querySelectorAll<HTMLButtonElement>('.js-choice');
+  const currentCoicedNumber = Number(clickedChoice.dataset.choiceNumber);
+
+  if (!currentQuestion) return;
+
+  const currentAnswerBlock = currentQuestion.querySelector<HTMLDivElement>('.js-answer');
+
+  const answeredQuestion = getAnsweredQuestion(currentQuestion, questions);
+  const isCorrect = answeredQuestion.correctAnswerNumber === getSelectedChoiceNumber(clickedChoice);
+
+  const enableSumbit = () => {
+    if (!submitButton) return;
+    submitButton.removeAttribute('disabled');
+  };
+
+  if (!currentAnswerBlock) return;
+
+  showAnswer(currentAnswerBlock);
+  updateJudgement(currentAnswerBlock, isCorrect);
+  highlightCorrectChoice(answeredQuestion, currentChoices);
+  highlightSelectedWrongChoice(clickedChoice, isCorrect);
+  disableChoices(currentChoices);
+  setResultData(currentCoicedNumber, resultData, answeredQuestion.questionId, isCorrect);
+
+  currentAnswerBlock.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+  if (Object.keys(resultData).length === questions.length) isComplete = true;
+  isComplete && enableSumbit();
+};
+
+const onClickSubmit = async () => {
+  const correctAnswersCount = getCorrectAnswersCount(resultData);
+  const correctRatio = correctAnswersCount / questions.length;
+  const isAllCorrect = correctRatio === 1;
+  const { messageContent, imageFileName } = getResultContent(correctRatio);
+
+  const disableSubmit = () => {
+    if (!submitButton) return;
+    submitButton.setAttribute('disabled', 'disabled');
+  };
+
+  disableSubmit();
+
+  if (
+    !correctAnswersBlock ||
+    !correctAnswersNumberSpan ||
+    !allCorrectBlock ||
+    !messageBlock ||
+    !illustrationImage ||
+    !resultBlock ||
+    !resultContentBlock
+  )
+    return;
+
+  if (isAllCorrect) {
+    hideElement(correctAnswersBlock);
+  } else {
+    hideElement(allCorrectBlock);
+    updateCorrectAnswersNumberSpan(correctAnswersNumberSpan, correctAnswersCount);
+  }
+
+  updateMessageBlock(messageContent, messageBlock);
+  await updateIllustration(isAllCorrect, imageFileName, illustrationImage);
+  showResult(isAllCorrect, resultBlock, resultContentBlock);
+  resultBlock.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+  setBrowserId();
+  POST_ENDPOINT && sendData(resultData, POST_ENDPOINT);
+};
+
+const onClickReset = () => {
+  if (!correctAnswersBlock || !illustrationImage || !resultBlock || !resultContentBlock || !allCorrectBlock) return;
+
+  resetAnswers(answerBlocks);
+  resetChoices(choices);
+  resetCorrectAnswersResultkChildren(correctAnswersBlock);
+  resetIllustration(illustrationImage);
+  resetResultBlock(resultBlock);
+  resetResultContentBlock(resultContentBlock);
+  showElement(allCorrectBlock);
+  showElement(correctAnswersBlock);
+  enableChoices(choices);
+  container!.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+  resultData = {};
+  isComplete = false;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener('click', ({ currentTarget }) => {
+    if (!(currentTarget instanceof HTMLButtonElement)) return;
+    onClickChoice(currentTarget);
+  });
+});
+
+submitButton?.addEventListener('click', (event) => {
+  event.preventDefault();
+  onClickSubmit();
+});
+
+resetButton?.addEventListener('click', () => {
+  onClickReset();
+});
---
+++ src/scripts/en/quiz/quizResult.ts
@@ -0,0 +1,104 @@
+const illustrationParentClassNames = ['!-bottom-16', 'md:!justify-center', 'md:!-bottom-24'];
+
+const resultContent = {
+  all: {
+    messageContent: 'すごーい！',
+    imageFileName: 'all',
+  },
+  above80: {
+    messageContent: 'よくできましたね！',
+    imageFileName: 'sumiki',
+  },
+  above40: {
+    messageContent: `あとちょっとで<br />もっとよくなるぞ！`,
+    imageFileName: 'daiin',
+  },
+  below40: {
+    messageContent: `もうすこし<br />がんばりましょう!`,
+    imageFileName: 'konaka',
+  },
+};
+
+export const getResultContent = (correctRatio: number) => {
+  if (correctRatio === 1) {
+    return resultContent.all;
+  } else if (correctRatio >= 0.8) {
+    return resultContent.above80;
+  } else if (correctRatio >= 0.4) {
+    return resultContent.above40;
+  } else {
+    return resultContent.below40;
+  }
+};
+
+export const hideElement = (Element: HTMLElement) => {
+  Element.classList.add('hidden');
+};
+
+export const showElement = (Element: HTMLElement) => {
+  Element.classList.remove('hidden');
+};
+
+export const resetCorrectAnswersResultkChildren = (correctAnswersBlock: HTMLDivElement) => {
+  correctAnswersBlock.childNodes.forEach((node) => {
+    if (!(node instanceof HTMLElement)) return;
+    node.classList.remove('hidden');
+  });
+};
+
+export const resetResultBlock = (resultBlock: HTMLElement) => {
+  resultBlock.classList.remove('!flex', '!flex-col');
+};
+
+export const resetResultContentBlock = (resultContentBlock: HTMLDivElement) => {
+  if (!resultContentBlock) return;
+  resultContentBlock.classList.remove('md:!mb-[7.5rem]');
+};
+
+export const resetIllustration = (illustrationImage: HTMLImageElement) => {
+  illustrationImage.classList.remove('!max-w-[18.75rem]');
+  illustrationImage.parentElement?.classList.remove(...illustrationParentClassNames);
+};
+
+export const showResult = (isAllCorrect: boolean, resultBlock: HTMLElement, resultContentBlock: HTMLDivElement) => {
+  resultBlock.classList.add('!flex', '!flex-col');
+  isAllCorrect && resultContentBlock?.classList.add('md:!mb-[7.5rem]');
+};
+
+export const updateCorrectAnswersNumberSpan = (
+  correctAnswersNumberSpan: HTMLSpanElement,
+  correctAnswersCount: number
+) => {
+  correctAnswersNumberSpan.innerText = correctAnswersCount.toString();
+};
+
+export const updateIllustration = (
+  isAllCorrect: boolean,
+  imageFileName: string,
+  illustrationImage: HTMLImageElement
+) => {
+  return new Promise<void>((resolve) => {
+    if (isAllCorrect) {
+      illustrationImage.classList.add('!max-w-[18.75rem]');
+      illustrationImage.parentElement?.classList.add(...illustrationParentClassNames);
+    }
+
+    illustrationImage.src = `../assets/images/${imageFileName}-quiz.png`;
+    illustrationImage.onload = () => resolve();
+  });
+};
+
+export const updateJudgement = (answerBlock: HTMLElement, isCorrect: boolean) => {
+  const currentJudgement = answerBlock.querySelector('.js-judgement');
+  if (!currentJudgement) return;
+
+  currentJudgement.textContent = isCorrect ? '正解です！' : '残念、不正解です';
+  currentJudgement.classList.replace(
+    isCorrect ? 'text-zinc-600' : 'text-orange-600',
+    isCorrect ? 'text-orange-600' : 'text-zinc-600'
+  );
+};
+
+export const updateMessageBlock = (messageContent: string, messageBlock: HTMLDivElement) => {
+  messageBlock.innerHTML = messageContent;
+};
