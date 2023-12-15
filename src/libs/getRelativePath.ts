3baa938975c63d1c18f58dac45ca9982b9ec402c
Kite <ixkaito@gmail.com>
2023-12-16 00:13:50 +0900

    Remove unnecessary libs

--- src/libs/getRelativePath.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-export const getRelativePath = ({ from, to }: { from: string; to: string | undefined }) => {
-  if (typeof to === 'string' && to.startsWith('/')) {
-    return path.relative(from, to);
-  }
-
-  return to;
-};
