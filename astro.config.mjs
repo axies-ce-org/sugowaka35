b18fa98ae1d7fd70cb90736b658eedd9f4376c0a
osgsm <50955473+osgsm@users.noreply.github.com>
2024-10-04 15:16:24 +0900

    Remove postbuildIntegration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import relativeLinks from 'astro-relative-links';
-import postbuildIntegration from './src/integrations/postbuild';
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +16,5 @@ export default defineConfig({
       },
     }),
     relativeLinks(),
-    postbuildIntegration(),
   ],
 });
